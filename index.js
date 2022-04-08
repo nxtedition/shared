3f8417134993bdcbc194036d0897b29fda23cd7e
Robert Nagy <ronagy@icloud.com>
2022-04-08 12:41:28 +0200

    husky and lint-staged

--- index.js
+++ index.js
@@ -1,118 +1,117 @@
-const WRITE_INDEX = 0;
-const READ_INDEX = 1;
+const WRITE_INDEX = 0
+const READ_INDEX = 1
 
 export function alloc(size) {
   return {
     sharedState: new SharedArrayBuffer(8),
     sharedBuffer: new SharedArrayBuffer(size),
-  };
+  }
 }
 
 export async function reader({ sharedState, sharedBuffer }, cb) {
-  const state = new Int32Array(sharedState);
-  const buffer = Buffer.from(sharedBuffer);
+  const state = new Int32Array(sharedState)
+  const buffer = Buffer.from(sharedBuffer)
 
-  let readPos = 0;
-  let writePos = 0;
+  let readPos = 0
+  let writePos = 0
 
   while (true) {
-    const { async, value } = Atomics.waitAsync(state, WRITE_INDEX, writePos);
+    const { async, value } = Atomics.waitAsync(state, WRITE_INDEX, writePos)
     if (async) {
-      await value;
+      await value
     }
-    writePos = Atomics.load(state, WRITE_INDEX);
+    writePos = Atomics.load(state, WRITE_INDEX)
 
     while (readPos !== writePos) {
-      const len = buffer.readInt32LE(readPos);
+      const len = buffer.readInt32LE(readPos)
 
       if (len === -1) {
-        readPos = 0;
+        readPos = 0
       } else {
-        const raw = buffer.slice(readPos + 4, readPos + len);
-        readPos += len;
+        const raw = buffer.slice(readPos + 4, readPos + len)
+        readPos += len
 
-        await cb(raw);
+        await cb(raw)
       }
 
-      Atomics.store(state, READ_INDEX, readPos);
+      Atomics.store(state, READ_INDEX, readPos)
     }
 
-    Atomics.notify(state, READ_INDEX);
+    Atomics.notify(state, READ_INDEX)
   }
 }
 
 export function writer({ sharedState, sharedBuffer }) {
-  const state = new Int32Array(sharedState);
-  const buffer = Buffer.from(sharedBuffer);
-  const size = buffer.byteLength;
-  const queue = [];
+  const state = new Int32Array(sharedState)
+  const buffer = Buffer.from(sharedBuffer)
+  const size = buffer.byteLength
+  const queue = []
 
-  let readPos = 0;
-  let writePos = 0;
-  let flushing = null;
+  let readPos = 0
+  let writePos = 0
+  let flushing = null
 
   function tryWrite(...raw) {
-    readPos = Atomics.load(state, READ_INDEX);
+    readPos = Atomics.load(state, READ_INDEX)
 
-    const len = raw.reduce((len, buf) => len + buf.byteLength, 4);
+    const len = raw.reduce((len, buf) => len + buf.byteLength, 4)
 
     if (size - writePos < len + 4) {
       if (readPos < len + 4) {
-        return false;
+        return false
       }
 
-      buffer.writeInt32LE(-1, writePos);
-      writePos = 0;
+      buffer.writeInt32LE(-1, writePos)
+      writePos = 0
     } else {
-      const available =
-        writePos >= readPos ? size - writePos : readPos - writePos;
+      const available = writePos >= readPos ? size - writePos : readPos - writePos
 
       if (available < len + 4) {
-        return false;
+        return false
       }
     }
 
-    buffer.writeInt32LE(len, writePos);
-    writePos += 4;
+    buffer.writeInt32LE(len, writePos)
+    writePos += 4
 
     for (const buf of raw) {
-      buffer.set(buf, writePos);
-      writePos += buf.byteLength;
+      buffer.set(buf, writePos)
+      writePos += buf.byteLength
     }
 
-    Atomics.store(state, WRITE_INDEX, writePos);
-    Atomics.notify(state, WRITE_INDEX);
+    Atomics.store(state, WRITE_INDEX, writePos)
+    Atomics.notify(state, WRITE_INDEX)
 
-    return true;
+    return true
   }
 
   async function flush() {
     if (flushing) {
-      return flushing;
+      return flushing
     }
 
     while (queue.length) {
       while (!tryWrite(queue[0])) {
-        const { async, value } = Atomics.waitAsync(state, READ_INDEX, readPos);
+        const { async, value } = Atomics.waitAsync(state, READ_INDEX, readPos)
         if (async) {
-          await value;
+          await value
         }
       }
-      queue.shift();
+      queue.shift()
     }
 
-    flushing = null;
+    flushing = null
   }
 
   async function write(...raw) {
     if (!queue.length && tryWrite(...raw)) {
-      return;
+      return
     }
 
-    queue.push(Buffer.concat(raw));
+    queue.push(Buffer.concat(raw))
 
-    await (flushing ??= flush());
+    await (flushing ??= flush())
   }
 
-  return write;
+  return write
 }
