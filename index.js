4c624811266f73f0667e5c6056ceca71119b8c57
Robert Nagy <ronagy@icloud.com>
2022-08-01 16:26:59 +0200

    WIP

--- index.js
+++ index.js
@@ -68,6 +68,41 @@ export function writer({ sharedState, sharedBuffer }) {
   let writePos = 0
   let flushing = null
 
+  function acquire(len) {
+    if (!len) {
+      return Buffer.alloc(0)
+    }
+
+    readPos = Atomics.load(state, READ_INDEX)
+
+    assert(len < size / 2)
+
+    if (size - writePos < len + 4) {
+      if (readPos < len + 4) {
+        return false
+      }
+
+      buffer.writeInt32LE(-1, writePos)
+      writePos = 0
+    } else {
+      const available = writePos >= readPos ? size - writePos : readPos - writePos
+
+      if (available < len + 4) {
+        return false
+      }
+    }
+
+    return buffer.subarray(writePos + 4, writePos + 4 + len)
+  }
+
+  function release(len) {
+    buffer.writeInt32LE(len, writePos)
+    writePos += 4 + len
+
+    Atomics.store(state, WRITE_INDEX, writePos)
+    Atomics.notify(state, WRITE_INDEX)
+  }
+
   function tryWrite(...raw) {
     if (Array.isArray(raw[0])) {
       raw = raw[0]
@@ -77,50 +112,29 @@ export function writer({ sharedState, sharedBuffer }) {
       return true
     }
 
-    readPos = Atomics.load(state, READ_INDEX)
-
     let maxLen = 4
     for (const buf of raw) {
       maxLen += buf.byteLength ?? buf.length * 3
     }
 
-    assert(maxLen < size / 2)
-
-    if (size - writePos < maxLen + 4) {
-      if (readPos < maxLen + 4) {
-        return false
-      }
-
-      buffer.writeInt32LE(-1, writePos)
-      writePos = 0
-    } else {
-      const available = writePos >= readPos ? size - writePos : readPos - writePos
-
-      if (available < maxLen + 4) {
-        return false
-      }
+    const dst = acquire(maxLen)
+    if (!dst) {
+      return false
     }
 
-    const lenPos = writePos
-    writePos += 4
-
+    let pos = 0
     for (const buf of raw) {
       if (typeof buf === 'string') {
-        writePos += buffer.write(buf, writePos)
+        pos += buffer.write(buf, pos)
       } else {
-        buffer.set(buf, writePos)
-        writePos += buf.byteLength
+        buffer.set(buf, pos)
+        pos += buf.byteLength
       }
     }
 
-    const len = writePos - lenPos
+    assert(pos <= maxLen)
 
-    assert(len <= maxLen)
-
-    buffer.writeInt32LE(len, lenPos)
-
-    Atomics.store(state, WRITE_INDEX, writePos)
-    Atomics.notify(state, WRITE_INDEX)
+    release(pos)
 
     return true
   }
@@ -148,6 +162,10 @@ export function writer({ sharedState, sharedBuffer }) {
 
     return (flushing ??= flush())
   }
+
+  write.acquire = acquire
+  write.release = release
+  write.flush = flush
 
   return write
 }
